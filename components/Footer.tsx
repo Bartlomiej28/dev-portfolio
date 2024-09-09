1ff1dd63a42303db39680d62ad0389555122029a
Bartłomiej Boczyło <120282905+Bartlomiej28@users.noreply.github.com>
2024-09-09 13:19:50 +0200

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,12 +4,12 @@ function Footer() {
   return (
     <footer className='border '>
         <div className="container flex flex-col p-4 mx-auto md:p-8 lg:flex-row dark:divide-gray-600">
-            <ul className="self-center py-6 space-y-4 text-center sm:flex sm:space-y-0 sm:justify-around sm:space-x-4 lg:flex-1 lg:justify-start gap-4">
+            <div className="flex flex-row gap-4">
                 <a href='#'>Home</a>
-                <a href='#about-me'>About</a>
                 <a href='#projects'>Projects</a>
+                <a href='#about-me'>About</a>
                 <a href='#contact'>Contact</a>
-            </ul>
+            </div>
             <div className="flex flex-row justify-center pt-6 lg:pt-0">
                 <div className="flex justify-center space-x-4">
                     <a rel="noopener noreferrer" href="https://www.linkedin.com/in/bart%C5%82omiej-boczy%C5%82o-71a275328/" title="LinkedIn" className="flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10">
@@ -20,8 +20,7 @@ function Footer() {
                    <a rel="noopener noreferrer" href="https://www.github.com/Bartlomiej28" title="GitHub" className="flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10">
                          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg>    
                    </a>
-               </div>
-              
+               </div>  
             </div>
         </div>
         <div className='w-full flex justify-center py-4'>
