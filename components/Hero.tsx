b47b4f35db05a6acb0cb3736539283f8f7c4b6b8
Bartłomiej Boczyło <120282905+Bartlomiej28@users.noreply.github.com>
2024-09-09 14:26:25 +0200

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import { AuroraBackground } from "@/components/ui/aurora-background";
 import zdjecie from '@/public/zdjecie.jpg';
 import Image from "next/image";
-import cv from "./Bartłomiej Boczyło CV"
+import cv from '@/public/cv.png'
   
 export function Hero() {
   return (
